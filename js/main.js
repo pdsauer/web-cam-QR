46da62d544568a908a8b7f1204f8a921d58ce6bb
Philipp Sauer <pdsauer@live.com>
2020-06-02 12:53:58 +0200

    added defer to script tag

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 // Elements for taking the snapshot
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+var canvasContext = document.getElementById('canvas').getContext('2d');
 var video = document.getElementById('video');
 let start = document.querySelector('#start').addEventListener('click', () => {
 	cameraRoll();
 });
 // Trigger photo take
-document.getElementById('snap').addEventListener('click', function () {
-	context.drawImage(video, 0, 0, 640, 480);
+document.getElementById('snap').addEventListener('click', () => {
+	canvasContext.drawImage(video, 0, 0, 640, 480);
 });
 const cameraRoll = () => {
 	if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
