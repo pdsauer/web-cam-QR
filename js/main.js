3ac51987f7ff1b0502e773376f42e30c53c4bd3b
Philipp Sauer <pdsauer@live.com>
2020-06-02 13:47:32 +0200

    changed to user

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ const cameraRoll = () => {
 				video: {
 					width: { ideal: 680 },
 					height: { ideal: 680 },
-					facingMode: facingmode,
+					facingMode: 'user',
 				},
 			})
 			.then(function (stream) {
