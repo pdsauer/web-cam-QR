ba513a12402201bce7eb04bb8142de58510f7862
Philipp Sauer <pdsauer@live.com>
2020-06-02 13:40:16 +0200

    changed facing mode to user

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ const cameraRoll = () => {
 				video: {
 					width: { ideal: 680 },
 					height: { ideal: 680 },
-					facingMode: { ideal: 'environment' },
+					facingMode: 'user',
 				},
 			})
 			.then(function (stream) {
